f7f40ca8c92813a04e91ca251b4956de76f7a7f5
Ahmed Alsammarraie <hamada.alsamrai@gmail.com>
2017-07-23 02:07:38 -0400

    Add config file

---
+++ src/config/hipchat.php
@@ -0,0 +1,56 @@
+<?php
+return array(
+    /*
+    |--------------------------------------------------------------------------
+    | HipChat Url
+    |--------------------------------------------------------------------------
+    */
+    'server' => env('HIPCHAT_URL'),
+    /*
+    |--------------------------------------------------------------------------
+    | HipChat API Token
+    |--------------------------------------------------------------------------
+    |
+    | Required API Token from HipChat
+    |
+    */
+    'api_token' => env('HIPCHAT_TOKEN'), // this is required
+
+    /*
+    |--------------------------------------------------------------------------
+    | HipChat Default Message color
+    |--------------------------------------------------------------------------
+    |
+    | Available options
+    |
+    |  yellow, green, red, purple, gray, random.
+    |
+    */
+    'color' => 'green',
+     /*
+    |--------------------------------------------------------------------------
+    | HipChat Default Notify
+    |--------------------------------------------------------------------------
+    | Sends a notification to room users if set to true (default).
+    */
+    'notify' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | HipChat Default Message Format
+    |--------------------------------------------------------------------------
+    |
+    | Available options
+    |
+    | html - Message is rendered as HTML and receives no special treatment. Must be valid HTML and entities must be escaped (e.g.: '&amp;' instead of '&').
+    |        May contain basic tags: a, b, i, strong, em, br, img, pre, code, lists, tables. Please see below for a list of allowed tags and attributes.
+    | text - Message is treated just like a message sent by a user. Can include @mentions, emoticons, pastes, and auto-detected URLs (Twitter, YouTube, images, etc).
+    |
+    */
+    'message_format' => 'text',
+    /*
+    |--------------------------------------------------------------------------
+    | HipChat Default Message
+    |--------------------------------------------------------------------------
+    */
+    'message' => 'My first notification (yey)'
+);
